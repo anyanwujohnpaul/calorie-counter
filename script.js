9b1ddfb6a0d4e5496889fc91c96af73799ab94f0
kvngjaypee <johnpaulanyanwu17@gmail.com>
2024-10-11 01:53:34 +0100

    completed the project and updated the js file

--- script.js
+++ script.js
@@ -105,6 +105,9 @@ function clearForm() {
   const inputContainers = Array.from(
     document.querySelectorAll(".input-container")
   );
+  for (const container of inputContainers) {
+    container.innerHTML = "";
+  }
 }
 
 addEntryButton.addEventListener("click", addEntry);
